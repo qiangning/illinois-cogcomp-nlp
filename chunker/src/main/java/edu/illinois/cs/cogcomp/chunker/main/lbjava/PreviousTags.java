bca7f070ba762e5bb8f9de1f013dce08ee974d56
Qiang <qning2@illinois.edu>
2018-08-14 02:18:37 -0500

    lbjava re-generated for partial chunker

--- chunker/src/main/java/edu/illinois/cs/cogcomp/chunker/main/lbjava/PreviousTags.java
+++ chunker/src/main/java/edu/illinois/cs/cogcomp/chunker/main/lbjava/PreviousTags.java
@@ -1,80 +1,88 @@
-/**
- * This software is released under the University of Illinois/Research and Academic Use License. See
- * the LICENSE file in the root folder for details. Copyright (c) 2016
- *
- * Developed by: The Cognitive Computation Group University of Illinois at Urbana-Champaign
- * http://cogcomp.cs.illinois.edu/
- */
+// Modifying this comment will cause the next execution of LBJava to overwrite this file.
+// F1B8800000000000000055D8BCA02C030154F75E671A4B84B41796450F5BB22E2417D157AA38121D425B288FFE6AA51499DCC3ECD337067B7B4E9A38595AB1B9ACDA5E1D5CB63762D8AD8D342816A87085B70B04BB7C8ED73182D854CC16CC5081364AD747BB8A3BB4B2344328EA49218435E4287A98E7CE9CF0620FCF8A44002AFACFF95D477FD818B44C15D9929B32591D5B29AC5C333BC2A8E7E97EB296D19DD2BF358EC3B459D6695C46AB82613F4A90B32DE80CDBE16086D1DCFE05F271D2CE9190100000
+
 package edu.illinois.cs.cogcomp.chunker.main.lbjava;
 
-import edu.illinois.cs.cogcomp.chunker.main.ChunkerConfigurator;
-import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
-import edu.illinois.cs.cogcomp.lbjava.classify.Classifier;
-import edu.illinois.cs.cogcomp.lbjava.classify.DiscretePrimitiveStringFeature;
-import edu.illinois.cs.cogcomp.lbjava.classify.FeatureVector;
+import edu.illinois.cs.cogcomp.chunker.utils.CoNLL2000ParserWithPartial;
+import edu.illinois.cs.cogcomp.lbjava.classify.*;
+import edu.illinois.cs.cogcomp.lbjava.infer.*;
+import edu.illinois.cs.cogcomp.lbjava.io.IOUtilities;
+import edu.illinois.cs.cogcomp.lbjava.learn.*;
+import edu.illinois.cs.cogcomp.lbjava.nlp.*;
 import edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token;
+import edu.illinois.cs.cogcomp.lbjava.parse.*;
+import edu.illinois.cs.cogcomp.pos.lbjava.POSTagger;
+import edu.illinois.cs.cogcomp.pos.lbjava.POSWindow;
 
 
 /**
- * Feature generator that senses the chunk tags of the previous two words. During training, labels
- * are present, so the previous two chunk tags are simply read from the data. Otherwise, the
- * prediction of the {@link Chunker} is used.
- *
- * @author Nick Rizzolo
+  * Feature generator that senses the chunk tags of the previous two words.
+  * During training, labels are present, so the previous two chunk tags are
+  * simply read from the data.  Otherwise, the prediction of the
+  * {@link Chunker} is used.
+  *
+  * @author Nick Rizzolo
  **/
-public class PreviousTags extends Classifier {
-    private static ResourceManager rm = new ChunkerConfigurator().getDefaultConfig();
-    private static String modelFile = rm.getString("modelPath");
-    private static String modelLexFile = rm.getString("modelLexPath");
-    private static final Chunker __Chunker = new Chunker(modelFile, modelLexFile);
+public class PreviousTags extends Classifier
+{
+  public PreviousTags()
+  {
+    containingPackage = "edu.illinois.cs.cogcomp.chunker.main.lbjava";
+    name = "PreviousTags";
+  }
 
-    public PreviousTags() {
-        containingPackage = "edu.illinois.cs.cogcomp.chunker.main.lbjava";
-        name = "PreviousTags";
+  public String getInputType() { return "edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token"; }
+  public String getOutputType() { return "discrete%"; }
+
+  public FeatureVector classify(Object __example)
+  {
+    if (!(__example instanceof Token))
+    {
+      String type = __example == null ? "null" : __example.getClass().getName();
+      System.err.println("Classifier 'PreviousTags(Token)' defined on line 19 of chunk_partial.lbj received '" + type + "' as input.");
+      new Exception().printStackTrace();
+      System.exit(1);
     }
 
-    public String getInputType() {
-        return "edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token";
+    Token word = (Token) __example;
+
+    FeatureVector __result;
+    __result = new FeatureVector();
+    String __id;
+    String __value;
+
+    int i;
+    Token w = word;
+    for (i = 0; i > -2 && w.previous != null; --i)
+    {
+      w = (Token) w.previous;
+    }
+    for (; w != word; w = (Token) w.next)
+    {
+      if (!w.label.equals(CoNLL2000ParserWithPartial.UNLABELED))
+      {
+        __id = "" + (i++);
+        __value = "" + (w.label);
+        __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name, __id, __value, valueIndexOf(__value), (short) 0));
+      }
+    }
+    return __result;
+  }
+
+  public FeatureVector[] classify(Object[] examples)
+  {
+    if (!(examples instanceof Token[]))
+    {
+      String type = examples == null ? "null" : examples.getClass().getName();
+      System.err.println("Classifier 'PreviousTags(Token)' defined on line 19 of chunk_partial.lbj received '" + type + "' as input.");
+      new Exception().printStackTrace();
+      System.exit(1);
     }
 
-    public String getOutputType() {
-        return "discrete%";
-    }
+    return super.classify(examples);
+  }
 
-    public FeatureVector classify(Object __example) {
-        Token word = (Token) __example;
-
-        FeatureVector __result;
-        __result = new FeatureVector();
-        String __id;
-        String __value;
-
-        int i;
-        Token w = word;
-        for (i = 0; i > -2 && w.previous != null; --i) {
-            w = (Token) w.previous;
-        }
-        for (; w != word; w = (Token) w.next) {
-            if (Chunker.isTraining) {
-                __id = "" + (i++);
-                __value = w.label;
-                __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage,
-                        this.name, __id, __value, valueIndexOf(__value), (short) 0));
-            } else {
-                __id = "" + (i++);
-                __value = __Chunker.discreteValue(w);
-                __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage,
-                        this.name, __id, __value, valueIndexOf(__value), (short) 0));
-            }
-        }
-        return __result;
-    }
-
-    public int hashCode() {
-        return "PreviousTags".hashCode();
-    }
-
-    public boolean equals(Object o) {
-        return o instanceof PreviousTags;
-    }
+  public int hashCode() { return "PreviousTags".hashCode(); }
+  public boolean equals(Object o) { return o instanceof PreviousTags; }
 }
+
--- chunker/src/main/java/edu/illinois/cs/cogcomp/chunker/main/lbjava/SOPrevious.java
+++ chunker/src/main/java/edu/illinois/cs/cogcomp/chunker/main/lbjava/SOPrevious.java
@@ -1,87 +1,103 @@
-/**
- * This software is released under the University of Illinois/Research and Academic Use License. See
- * the LICENSE file in the root folder for details. Copyright (c) 2016
- *
- * Developed by: The Cognitive Computation Group University of Illinois at Urbana-Champaign
- * http://cogcomp.cs.illinois.edu/
- */
+// Modifying this comment will cause the next execution of LBJava to overwrite this file.
+// F1B880000000000000005A15D5B43C0341DFB27A5074B49696BEB91728F1B7B2BD0639D3C8225D56538525D425B28CEFBB946D5DA34F146F21E427FED3F173B162F940554FC0B89DC50D7765732DB56DF2493ADA5C6C7C5688F403EA0C80AB77C8D6D806B5B087C4F5362068B2489264324B1DB6744076C0ED4551148123F53F891DD3BBF386328D161A403E5EA378A2A49ABB83D6F0F67E939FE2755C32DAE8A125DD066C6D17EAB06152D3A4D027D8B079473DBFEBE0723CA7795C6E22ADDA3784AF3D837AF1AC4C1B20CC01CC76B85615694587DAFDBC8DA4B19DB96EE020314FFA916B587E4FEF22AF6D41594FE6BE96695A17C1FCB0129A85135FCA19265451DDF43BBEB994639B3FDC2DD9E3ECFB64D4A0949B4A075F2007138D05007F1C5D76F4C6CDF90E27825A29FD5BABFBB365A31249A470A49C1925A65967F5B777296B40300000
+
 package edu.illinois.cs.cogcomp.chunker.main.lbjava;
 
-import edu.illinois.cs.cogcomp.chunker.main.ChunkerConfigurator;
-import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
-import edu.illinois.cs.cogcomp.lbjava.classify.Classifier;
-import edu.illinois.cs.cogcomp.lbjava.classify.DiscretePrimitiveStringFeature;
-import edu.illinois.cs.cogcomp.lbjava.classify.FeatureVector;
+import edu.illinois.cs.cogcomp.chunker.utils.CoNLL2000ParserWithPartial;
+import edu.illinois.cs.cogcomp.lbjava.classify.*;
+import edu.illinois.cs.cogcomp.lbjava.infer.*;
+import edu.illinois.cs.cogcomp.lbjava.io.IOUtilities;
+import edu.illinois.cs.cogcomp.lbjava.learn.*;
+import edu.illinois.cs.cogcomp.lbjava.nlp.*;
 import edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token;
+import edu.illinois.cs.cogcomp.lbjava.parse.*;
 import edu.illinois.cs.cogcomp.pos.lbjava.POSTagger;
+import edu.illinois.cs.cogcomp.pos.lbjava.POSWindow;
 
 
-public class SOPrevious extends Classifier {
-    private static ResourceManager rm = new ChunkerConfigurator().getDefaultConfig();
-    private static String modelFile = rm.getString("modelPath");
-    private static String modelLexFile = rm.getString("modelLexPath");
-    private static final Chunker __Chunker = new Chunker(modelFile, modelLexFile);
-    private static final POSTagger __POSTagger = new POSTagger();
+public class SOPrevious extends Classifier
+{
+  private static final POSTagger __POSTagger = new POSTagger();
 
-    public SOPrevious() {
-        containingPackage = "edu.illinois.cs.cogcomp.chunker.main.lbjava";
-        name = "SOPrevious";
+  public SOPrevious()
+  {
+    containingPackage = "edu.illinois.cs.cogcomp.chunker.main.lbjava";
+    name = "SOPrevious";
+  }
+
+  public String getInputType() { return "edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token"; }
+  public String getOutputType() { return "discrete%"; }
+
+  public FeatureVector classify(Object __example)
+  {
+    if (!(__example instanceof Token))
+    {
+      String type = __example == null ? "null" : __example.getClass().getName();
+      System.err.println("Classifier 'SOPrevious(Token)' defined on line 33 of chunk_partial.lbj received '" + type + "' as input.");
+      new Exception().printStackTrace();
+      System.exit(1);
     }
 
-    public String getInputType() {
-        return "edu.illinois.cs.cogcomp.lbjava.nlp.seg.Token";
+    Token word = (Token) __example;
+
+    FeatureVector __result;
+    __result = new FeatureVector();
+    String __id;
+    String __value;
+
+    int i;
+    Token w = word;
+    for (i = 0; i > -2 && w.previous != null; --i)
+    {
+      w = (Token) w.previous;
+    }
+    String[] tags = new String[3];
+    String[] labels = new String[2];
+    tags[0] = tags[1] = tags[2] = "null";
+    labels[0] = labels[1] = "null";
+    i = 0;
+    for (; w != word; w = (Token) w.next)
+    {
+      tags[i] = __POSTagger.discreteValue(w);
+      labels[i] = w.label;
+      i++;
+    }
+    tags[i] = __POSTagger.discreteValue(w);
+    if (!labels[0].equals(CoNLL2000ParserWithPartial.UNLABELED) && !labels[1].equals(CoNLL2000ParserWithPartial.UNLABELED))
+    {
+      __id = "ll";
+      __value = "" + (labels[0] + "_" + labels[1]);
+      __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name, __id, __value, valueIndexOf(__value), (short) 0));
+    }
+    if (!labels[0].equals(CoNLL2000ParserWithPartial.UNLABELED))
+    {
+      __id = "lt1";
+      __value = "" + (labels[0] + "_" + tags[1]);
+      __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name, __id, __value, valueIndexOf(__value), (short) 0));
+    }
+    if (!labels[1].equals(CoNLL2000ParserWithPartial.UNLABELED))
+    {
+      __id = "lt2";
+      __value = "" + (labels[1] + "_" + tags[2]);
+      __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name, __id, __value, valueIndexOf(__value), (short) 0));
+    }
+    return __result;
+  }
+
+  public FeatureVector[] classify(Object[] examples)
+  {
+    if (!(examples instanceof Token[]))
+    {
+      String type = examples == null ? "null" : examples.getClass().getName();
+      System.err.println("Classifier 'SOPrevious(Token)' defined on line 33 of chunk_partial.lbj received '" + type + "' as input.");
+      new Exception().printStackTrace();
+      System.exit(1);
     }
 
-    public String getOutputType() {
-        return "discrete%";
-    }
+    return super.classify(examples);
+  }
 
-    public FeatureVector classify(Object __example) {
-        Token word = (Token) __example;
-
-        FeatureVector __result;
-        __result = new FeatureVector();
-        String __id;
-        String __value;
-
-        int i;
-        Token w = word;
-        for (i = 0; i > -2 && w.previous != null; --i) {
-            w = (Token) w.previous;
-        }
-        String[] tags = new String[3];
-        String[] labels = new String[2];
-        i = 0;
-        for (; w != word; w = (Token) w.next) {
-            tags[i] = __POSTagger.discreteValue(w);
-            if (Chunker.isTraining) {
-                labels[i] = w.label;
-            } else {
-                labels[i] = __Chunker.discreteValue(w);
-            }
-            i++;
-        }
-        tags[i] = __POSTagger.discreteValue(w);
-        __id = "ll";
-        __value = "" + (labels[0] + "_" + labels[1]);
-        __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name,
-                __id, __value, valueIndexOf(__value), (short) 0));
-        __id = "lt1";
-        __value = "" + (labels[0] + "_" + tags[1]);
-        __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name,
-                __id, __value, valueIndexOf(__value), (short) 0));
-        __id = "lt2";
-        __value = "" + (labels[1] + "_" + tags[2]);
-        __result.addFeature(new DiscretePrimitiveStringFeature(this.containingPackage, this.name,
-                __id, __value, valueIndexOf(__value), (short) 0));
-        return __result;
-    }
-
-    public int hashCode() {
-        return "SOPrevious".hashCode();
-    }
-
-    public boolean equals(Object o) {
-        return o instanceof SOPrevious;
-    }
+  public int hashCode() { return "SOPrevious".hashCode(); }
+  public boolean equals(Object o) { return o instanceof SOPrevious; }
 }
+
